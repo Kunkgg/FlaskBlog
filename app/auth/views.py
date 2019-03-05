cee88611e47dbfd7640752debc27f800d937c0a3
gk07 <gk_520@hotmail.com>
2019-03-05 20:32:31 +0800

    Register

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,11 @@
 
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
+from wtforms import ValidationError
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .. import db
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -27,3 +29,29 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        if adduser(form):
+            user = User.query.filter_by(email=form.email.data).first()
+            if user:
+                login_user(user, True)
+            return redirect(url_for('main.index'))
+    return render_template('auth/login.html', form=form)    
+
+
+def adduser(form):
+    user = User(username=form.username.data,
+                email=form.email.data,
+                password=form.password.data)
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except:
+        flash('Database Error')
+        return False
+    else:
+        return True
