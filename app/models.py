63a1a740078b35f614764a24fc6b320f3209b617
gk07 <gk_520@hotmail.com>
2019-03-10 22:31:10 +0800

    User Profile

--- app/models.py
+++ app/models.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+from flask import request
 
 from . import db
 from . import login_manager
+
+DEFAULT_AVATAR_HASH = 'e75ad1fb14ac2f331d4423236ba55954'
 
 
 class Permission:
@@ -77,6 +82,14 @@ class User(UserMixin, db.Model):
     confirmed = db.Column(db.Boolean, default=False)
     # add password feild
     password_hash = db.Column(db.String(128))
+    # add profile feilds
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    # add avatar gavatar email hash
+    avatar_hash = db.Column(db.String(32))
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -84,6 +97,8 @@ class User(UserMixin, db.Model):
             self.role = Role.query.filter_by(name='Administrator').first()
         if self.role is None:
             self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = self.gravatar_hash()
 
     @property
     def password(self):
@@ -112,11 +127,44 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def change_email(self, token):
+        data = parse_token(token)
+        newemail = data.get('newemail')
+        if newemail:
+            current_user.email = newemail
+            self.avatar_hash = self.gravatar_hash()
+            db.session.add(current_user)
+            return True
+
     def can(self, perm):
         return self.role is not None and self.role.has_permission(perm)
 
     def is_administrator(self):
         return self.can(Permission.ADMIN)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+        db.session.commit()
+
+    def gravatar_hash(self):
+        return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or self.gravatar_hash()
+        return f'{url}/{hash}?s={size}&d={default}&r={rating}'
+
+    @staticmethod
+    def insert_default_avatar_hash(hash_string=DEFAULT_AVATAR_HASH):
+        for u in User.query.all():
+            if u.avatar_hash is None:
+                u.avatar_hash = hash_string
+                db.session.add(u)
+        db.session.commit()
 
     def __repr__(self):
         return '<User %r>' % self.username
